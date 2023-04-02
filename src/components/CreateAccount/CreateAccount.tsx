ca79308b501dda1d7d6330c65827e48ceb663582
pinreddysu <pinreddysu@gmail.com>
2023-04-02 16:47:48 -0500

    update text

--- src/components/CreateAccount/CreateAccount.tsx
+++ src/components/CreateAccount/CreateAccount.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const CreateAccount = () => {
   return (
     <div>
-      <h1>CreateAccount</h1>
+      <h1>CreateAccount!!</h1>
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
     </div>
